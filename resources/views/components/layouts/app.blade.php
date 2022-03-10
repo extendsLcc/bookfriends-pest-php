4410655881cd35b09311ea64a674f3dcdfa51289
extendsLcc <lucas.lcc@hotmail.com>
2022-03-10 00:43:03 -0300

    wip(layout): work in progress implementing base application layout

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bookfriends</title>
+
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}"></script>
+</head>
+<body>
+
+<div class="max-w-4xl mx-auto px-6 grid grid-cols-8 gap-12 mt-16">
+    <div class="col-span-2 border-r border-slate-200 space-y-6">
+        <ul>
+            <li>
+                <span class="font-bold text-lg text-slate-600 hover:text-slate-800 block py-1">Extends</span>
+            </li>
+            <li>
+                <a href="" class="fon font-bold text-lg text-slate-600 hover:text-slate-800 block py-1">Feed</a>
+            </li>
+        </ul>
+    </div>
+    <div class="col-span-6 ">
+
+        @isset($header)
+            <h1 class="font-bold text-2xl text-slate-800">
+                {{ $header }}
+            </h1>
+        @endisset
+
+        {{ $slot }}
+
+    </div>
+</div>
+
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<x-layouts.app>
+
+    <x-slot name="header">
+        head
+    </x-slot>
+
+    <div class="mt-9">
+        content
+    </div>
+
+</x-layouts.app>
