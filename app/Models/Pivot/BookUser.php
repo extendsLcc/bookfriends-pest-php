aa37a580f7d3fadd6db760afb206ca31ea419a11
extendslcc <lucas.lcc@hotmail.com>
2022-03-24 01:11:43 -0300

    feat(book): implements create user book endpoint

--- app/Models/Pivot/BookUser.php
+++ app/Models/Pivot/BookUser.php
@@ -6,11 +6,9 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class BookUser extends Pivot
 {
-
     public static array $statuses = [
         'WANT_TO_READ' => 'Want to read',
         'READING' => 'Reading',
         'READ' => 'Read',
     ];
-
 }
