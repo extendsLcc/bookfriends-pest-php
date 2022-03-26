dc9ea85799c2825763d79e0f75fb75cd14249168
extendslcc <lucas.lcc@hotmail.com>
2022-03-25 22:42:31 -0300

    test(books): test user can only see edit view of books he owns

--- tests/Feature/BookEditTest.php
+++ tests/Feature/BookEditTest.php
@@ -3,6 +3,7 @@
 use App\Models\Book;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Symfony\Component\HttpFoundation\Response;
 use function Pest\Laravel\actingAs;
 
 uses(RefreshDatabase::class);
@@ -21,4 +22,17 @@ it('shows book details in the form', function () {
         ->assertOk()
         ->assertSee([$book->title, $book->author])
         ->assertSee('<option value="READING" selected>Reading</option>', false);
+});
+
+it('fails if the user does not own the book', function () {
+    $bookOwnerUser = User::factory()->create();
+    $anotherUser = User::factory()->create();
+
+    $bookOwnerUser->books()->attach($book = Book::factory()->create(), [
+        'status' => 'READING',
+    ]);
+
+    actingAs($anotherUser)
+        ->get("/books/$book->id/edit")
+        ->assertStatus(Response::HTTP_FORBIDDEN);
 });
