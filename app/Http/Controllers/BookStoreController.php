b31c2e53e53ebfd60bb9e2a501850ed96177a5dd
extendslcc <lucas.lcc@hotmail.com>
2022-03-27 03:24:48 -0300

    refactor(books): remove redundant code from BookStoreController

--- app/Http/Controllers/BookStoreController.php
+++ app/Http/Controllers/BookStoreController.php
@@ -4,27 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\BookStoreRequest;
 use App\Models\Book;
-use App\Models\Pivot\BookUser;
-use Illuminate\Validation\Rule;
 
 class BookStoreController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware(['auth']);
-    }
-
     public function __invoke(BookStoreRequest $request)
     {
-        $this->validate($request, [
-            'title' => 'required',
-            'author' => 'required',
-            'status' => [
-                'required',
-                Rule::in(array_keys(BookUser::$statuses))
-            ],
-        ]);
-
         $book = Book::create($request->only('title', 'author'));
 
         $request->user()->books()->attach($book, [
